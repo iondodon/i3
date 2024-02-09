5558de01e9ea4fb5ca1c8689e41a1956be9aaafa
Ion Dodon <iondodon2@gmail.com>
2024-02-09 16:59:41 +0200

    Use script for rofi switcher

--- rofi.sh
+++ rofi.sh
@@ -1,9 +1,89 @@
-#!/bin/bash
-rofi -modi window -show window -hide-scrollbar -padding 50 -line-padding 4 -auto-select \
- -kb-cancel "Alt+Escape,Escape" \
- -kb-accept-entry "!Alt-Tab,!Alt+Alt_L,Return"\
- -kb-row-down "Alt+Tab,Alt+Down" \
- -kb-row-up "Alt+ISO_Left_Tab,Alt+Up" &
+#!/usr/bin/env bash
+set -euo pipefail
 
-xdotool search --sync --class Rofi keyup --delay 0 Tab & 
-xdotool key --delay 0 Tab &
+LOGFILE=""
+ROFI_ARGS=()
+KB_ACCEPT_APPEND=""
+MODE=""
+while getopts "m:l:" OPTION; do 
+    case "$OPTION" in
+        m)
+            MODE="$OPTARG"
+            case "$MODE" in
+                alttab)
+                    ROFI_ARGS+=( -show window -selected-row 1) # -selected-row is only available in https://github.com/davatorium/rofi/pull/909
+                    KB_ACCEPT_APPEND=",!Alt+Alt_L"
+                    ;;
+                altspace)
+                    ROFI_ARGS+=( -show window )
+                    ;;
+                superspace)
+                    ROFI_ARGS+=( -show drun )
+                    ;;
+                *)
+                    echo "unknown mode $MODE";
+                    exit 1 # todo use libnotify to display error
+                    ;;
+            esac
+            ;;
+        l)
+            LOGFILE="$OPTARG"
+            ;;
+        *)
+            echo "unknown option '$OPTION'"
+            exit 1
+            ;;
+    esac
+done
+
+if [ "$MODE" == "" ]; then
+    echo "must specify a mode"
+    exit 1
+fi
+
+if [ "$LOGFILE" != "" ]; then
+    echo "redirecting output to file"
+    exec >> "$LOGFILE"
+    exec 2>&1
+fi
+
+# Capture the cursor's original position
+eval $(xdotool getmouselocation --shell)
+
+# Launch rofi
+export ROFI_DUMP_WINDOW_POSITION="$(mktemp)"
+rofi \
+    -kb-cancel "Alt+Escape,Escape" \
+    -kb-accept-entry '!Alt-Tab,!Alt+Down,!Alt+ISO_Left_Tab,!Alt+Up,Return'$KB_ACCEPT_APPEND \
+    -kb-row-down 'Alt-Tab,Alt+Down,Down' \
+    -kb-row-up 'Alt+ISO_Left_Tab,Alt+Up,Up' \
+    "${ROFI_ARGS[@]}"&
+rofipid="$!"
+
+# The strategy to move the cursor into the rofi window immediately after it appears
+(
+    sleep 0.1 # Small delay to ensure rofi window is up
+    # Wait for the ROFI window to appear by monitoring its PID
+    while : ; do
+        if xdotool search --pid "$rofipid" > /dev/null 2>&1; then
+            break # Rofi window found
+        fi
+        sleep 0.1
+    done
+
+    # Now move the mouse to the center of the rofi window
+    WIN_ID=$(xdotool search --pid "$rofipid" | head -n 1)
+    eval $(xdotool getwindowgeometry --shell "$WIN_ID")
+    let "X+=WIDTH/2"
+    let "Y+=HEIGHT/2"
+    xdotool mousemove $X $Y
+) &
+
+# Wait for rofi to close
+wait "$rofipid"
+
+# Move the cursor back to its original position
+xdotool mousemove $X $Y
+
+# Clean up
+rm -f "$ROFI_DUMP_WINDOW_POSITION"
